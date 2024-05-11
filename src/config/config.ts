a8e085fa0cdae4300c96682f207e6f7180360b37
Paranoia8972 <contact@encryptopia.dev>
2024-05-11 21:49:20 +0200

    Edited config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,1 @@
-export const GITHUB_EDIT_URL = `https://github.com/mverissimo/next-galaxy-starter/edit/main/src/pages`;
+export const GITHUB_EDIT_URL = `https://github.com/Paranoia8972/docs/edit/main/src/pages`;
